bf0377b6efeb974972efc8c60abe616c8f4ab27b
k236740 <k236740@gmail.com>
2016-09-01 10:51:58 +0800

    sliderPro 檔案連結

---
+++ sliderPro/dist/js/jquery.sliderPro.config.js
@@ -0,0 +1,39 @@
+jQuery.noConflict();
+(function($) {
+    $(function() {
+      $( '#aideshop-article' ).sliderPro({
+        width: '100%',
+        height: 600,
+        orientation: 'horizontal',
+        loop: false,
+        arrows: true,
+        buttons: false,
+        autoplay:false,
+        touchSwipe:true,
+        arrows:true,
+        thumbnailsPosition: 'bottom',
+        thumbnailPointer: true,
+        thumbnailWidth: 350,
+        thumbnailHeight:170,
+        imageScaleMode:'cover',
+        fade:true,
+        breakpoints: {
+          1200: {
+            thumbnailsPosition: 'bottom',
+            thumbnailWidth: 350,
+            thumbnailHeight: 170
+          },
+          800: {
+            thumbnailsPosition: 'bottom',
+            thumbnailWidth: 350,
+            thumbnailHeight: 170
+          },
+          500: {
+            thumbnailsPosition: 'bottom',
+            thumbnailWidth: 120,
+            thumbnailHeight: 170
+          }
+        }
+      });
+    });
+})(jQuery);
