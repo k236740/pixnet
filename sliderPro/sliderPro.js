c6658cc98816dc73dabc3aef1b95bd2b9d47a83c
k236740 <k236740@gmail.com>
2016-09-03 22:36:31 +0800

    0

---
+++ sliderPro/sliderPro.js
@@ -0,0 +1,5 @@
+
+$.include("https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js");
+$.include("https://k236740.github.io/pixnet/sliderPro/dist/css/slider-pro.min.css");
+$.include("https://k236740.github.io/pixnet/sliderPro/dist/js/jquery.sliderPro.min.js");
+$.include("https://k236740.github.io/pixnet/sliderPro/dist/js/jquery.sliderPro.config.js");
