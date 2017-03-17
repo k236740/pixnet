dba68da987034aa020f62509159f1453e20af05d
k236740 <k236740@gmail.com>
2017-03-17 18:23:11 +0800

    0

--- aideshop/js/youtube_panel/js/slide.js
+++ aideshop/js/youtube_panel/js/slide.js
@@ -3,14 +3,14 @@
       $('<audio controls id="au-002"><source src="https://k236740.github.io/pixnet/aideshop/js/youtube_panel/002.ogg" type="audio/ogg"><source src="https://k236740.github.io/pixnet/aideshop/js/youtube_panel/002.mp3" type="audio/mpeg"></audio>').appendTo('body');
   		$('<audio controls id="au-003"><source src="https://k236740.github.io/pixnet/aideshop/js/youtube_panel/003.ogg" type="audio/ogg"><source src="https://k236740.github.io/pixnet/aideshop/js/youtube_panel/003.mp3" type="audio/mpeg"></audio>').appendTo('body');
       // Expand Panel
-      $("#as-youtube-open").click(function() {
+      $("#as-youtube-open,#ay-open").click(function() {
           $("div#as-youtube-panel").slideDown("slow");
           $('#au-002')[0].play(); //播放聲音
           // $.playSound("002");
       });
 
       // Collapse Panel
-      $("#as-youtube-close").click(function() {
+      $("#as-youtube-close,#ay-close").click(function() {
           $("div#as-youtube-panel").slideUp("slow");
           $('#au-003')[0].play(); //播放聲音
           // $.playSound("003");
