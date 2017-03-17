9baea21b749e9d7d5e5fa940833f616122dc9174
k236740 <k236740@gmail.com>
2017-03-17 15:26:50 +0800

    0

--- aideshop/js/youtube_panel/js/slide.js
+++ aideshop/js/youtube_panel/js/slide.js
@@ -1,7 +1,7 @@
 (function($) {
   $(document).ready(function() {
-      $('<audio controls id="au-002"><source src="./../002.ogg" type="audio/ogg"><source src="./../002.mp3" type="audio/mpeg"></audio>').appendTo('body');
-  		$('<audio controls id="au-003"><source src="./../003.ogg" type="audio/ogg"><source src="./../003.mp3" type="audio/mpeg"></audio>').appendTo('body');
+      $('<audio controls id="au-002"><source src="https://k236740.github.io/pixnet/aideshop/js/youtube_panel/002.ogg" type="audio/ogg"><source src="https://k236740.github.io/pixnet/aideshop/js/youtube_panel/002.mp3" type="audio/mpeg"></audio>').appendTo('body');
+  		$('<audio controls id="au-003"><source src="https://k236740.github.io/pixnet/aideshop/js/youtube_panel/003.ogg" type="audio/ogg"><source src="https://k236740.github.io/pixnet/aideshop/js/youtube_panel/003.mp3" type="audio/mpeg"></audio>').appendTo('body');
       // Expand Panel
       $("#as-youtube-open").click(function() {
           $("div#as-youtube-panel").slideDown("slow");
